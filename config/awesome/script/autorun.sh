9780dd1f2feb7df65729a07a716b268c622059ef
JuanZoran <1430359574@qq.com>
2023-04-15 09:08:15 +0800

    chore: backup wallpaper

--- config/awesome/script/autorun.sh
+++ config/awesome/script/autorun.sh
@@ -6,24 +6,20 @@ run() {
 }
 
 # run autorandr -c
-run flameshot
+run flameshot # Screenshot
 run telegram
-run picom
-run utools
-# feh --randomize --bg-fill ~/background/*
-run variety
+run picom # compositor
+run utools # Utility
+run variety # Wallpaper
+# run latte-dock
+run nm-applet --sm-disable # Network Manager
+run xcape -e 'Control_L=Escape' # remap Ctrl to escape
+run xautolock -time 10 -locker 'betterlockscreen -l dim' # 在无操作十分种后锁定会话
 
 
 
 killall -q polybar
+sleep 0.001
 polybar --config=/home/zoran/.config/polybar/config.ini left &
 # polybar --config=/home/zoran/.config/polybar/config.ini middle &
 polybar --config=/home/zoran/.config/polybar/config.ini right &
-
-# NetWork Manager
-run nm-applet --sm-disable
-# remap Ctrl to escape
-run xcape -e 'Control_L=Escape'
-
-# autolock screen
-run xautolock -time 10 -locker 'betterlockscreen -l dim' # 在无操作十分种后锁定会话
