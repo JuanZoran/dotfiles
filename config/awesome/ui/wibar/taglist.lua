c232ef179fe068ec06d9b0fb5f5aec5f7d059880
JuanZoran <1430359574@qq.com>
2023-06-04 12:08:29 +0800

    refactor: export submodule: awesome, background, surfingkeys-conf

--- config/awesome/ui/wibar/taglist.lua
+++ config/awesome/ui/wibar/taglist.lua
@@ -1,39 +1,16 @@
-local dpi = require 'beautiful.xresources'.apply_dpi
-bling.widget.tag_preview.enable {
-    show_client_content = true, -- Whether or not to show the client content
-    scale = 0.25,               -- The scale of the previews compared to the screen
-    honor_padding = true,       -- Honor padding when creating widget size
-    honor_workarea = true,      -- Honor work area when creating widget size
-    placement_fn = function(c)  -- Place the widget using awful.placement (this overrides x & y)
-        awful.placement.next_to_mouse(c, {
-            offset = {
-                y = dpi(60),
-            },
-        })
-    end,
-}
-
-local c       = beautiful.color
----@see https://awesomewm.org/apidoc/widgets/awful.widget.taglist.html#awful.widget.taglist
 ----------------------------------------------------------------------
-local btn_act = util.enum.bottom
-local button  = util.button
+local awful  = require 'awful'
+local rubato = require 'lib.rubato'
+local c      = beautiful.color
+local btn    = awful.button.names
+local button = util.button
+local dpi    = require 'beautiful.xresources'.apply_dpi
 ----------------------------------------------------------------------
 local buttons = util.keys {
-    button { btn_act.LEFT, function(t) t:view_only() end },
-    button { mods = { modkey }, 1, function(t)
-        if client.focus then
-            client.focus:move_to_tag(t)
-        end
-    end, },
-    button { mods = { modkey }, 3, function(t)
-        if client.focus then
-            client.focus:toggle_tag(t)
-        end
-    end, },
-    button { btn_act.RIGHT, awful.tag.viewtoggle },
-    button { btn_act.SCROLL_DOWN, function(t) awful.tag.viewnext(t.screen) end },
-    button { btn_act.SCROLL_UP, function(t) awful.tag.viewprev(t.screen) end },
+    button { btn.LEFT, function(t) t:view_only() end },
+    button { btn.RIGHT, awful.tag.viewtoggle },
+    button { btn.SCROLL_DOWN, function(t) awful.tag.viewnext(t.screen) end },
+    button { btn.SCROLL_UP, function(t) awful.tag.viewprev(t.screen) end },
 }
 
 local icon    = {
@@ -43,14 +20,11 @@ local icon    = {
 }
 
 return function(s)
-    awful.tag( -- { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-        { 1, 2, 3, 4 },
-        s, awful.layout.layouts[1])
+    awful.tag({ 1, 2, 3, 4 }, s, awful.layout.layouts[1])
 
     local function set_icon(self, t)
         local container = self:get_children_by_id 'icon'[1]
         local text_widget = container.widget
-        -- print(text_widget)
         if t.selected then
             text_widget.text = icon.active[1]
             container.fg = icon.active[2]
@@ -61,21 +35,6 @@ return function(s)
             text_widget.text = icon.empty[1]
             container.fg = icon.empty[2]
         end
-    end
-
-
-    local create_callback = function(self, t, index, objects) --luacheck: no unused args
-        set_icon(self, t)
-        self:connect_signal('mouse::enter', function()
-            if #t:clients() > 0 then
-                awesome.emit_signal('bling::tag_preview::update', t)
-                awesome.emit_signal('bling::tag_preview::visibility', s, true)
-            end
-        end)
-
-        self:connect_signal('mouse::leave', function()
-            awesome.emit_signal('bling::tag_preview::visibility', s, false)
-        end)
     end
 
     return wibox.container.margin(awful.widget.taglist {
@@ -99,7 +58,15 @@ return function(s)
                 widget = wibox.container.background,
             },
             ---@diagnostic disable-next-line: unused-local
-            create_callback = create_callback,
+            create_callback = function(self, t, index, objects) --luacheck: no unused args
+                set_icon(self, t)
+                -- self.animate = rubato.timed {
+                --     duration = 0.15,
+                --     subscribed = function(w)
+                --         self:get_children_by_id 'icon'[1].forced_width = w
+                --     end,
+                -- }
+            end,
             update_callback = function(self, t, index, objects) --luacheck: no unused args
                 set_icon(self, t)
             end,
--- config/awesome/ui/wibar/tasklist.lua
+++ config/awesome/ui/wibar/tasklist.lua
@@ -1,20 +1,21 @@
+local btn = awful.button.names
+
 return function(s)
-    local btn_act = util.enum.bottom
     local button = util.button
     return awful.widget.tasklist {
         screen          = s,
         filter          = awful.widget.tasklist.filter.currenttags,
         buttons         = util.keys {
-            button { btn_act.LEFT, function(c)
+            button { btn.LEFT, function(c)
                 if c == client.focus then
                     c.minimized = true
                 else
                     c:emit_signal('request::activate', 'tasklist', { raise = true })
                 end
             end, },
-            button { btn_act.RIGHT, function() awful.menu.client_list { theme = { width = 250 } } end },
-            button { btn_act.SCROLL_DOWN, function() awful.client.focus.byidx(1) end },
-            button { btn_act.SCROLL_UP, function() awful.client.focus.byidx(-1) end },
+            button { btn.RIGHT, function() awful.menu.client_list { theme = { width = 250 } } end },
+            button { btn.SCROLL_DOWN, function() awful.client.focus.byidx(1) end },
+            button { btn.SCROLL_UP, function() awful.client.focus.byidx(-1) end },
         },
         layout          = {
             spacing_widget = {
